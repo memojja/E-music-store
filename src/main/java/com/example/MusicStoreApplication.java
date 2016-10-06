0f9b99ed932806051a14e1d577b4d6bb40c2da38
memojja <memojja1907@gmail.com>
2016-10-06 03:16:09 +0300

    initialize project

---
+++ src/main/java/com/example/MusicStoreApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicStoreApplication.class, args);
+	}
+}
