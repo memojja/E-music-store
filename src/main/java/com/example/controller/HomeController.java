a5cb494318914f8dc80016ba6c5d258a0eb15a86
memojja <memojja1907@gmail.com>
2016-10-12 15:56:41 +0300

    home page

---
+++ src/main/java/com/example/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by mehmet on 12.10.2016.
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = {"/","index","/home"})
+    public String home(Model model){
+        model.addAttribute("hello","Hello World!");
+        return "home";
+    }
+
+
+}
